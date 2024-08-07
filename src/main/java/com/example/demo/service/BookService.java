094d6ae9ec32320f26284e477b35a05aa30caac5
gonchigars <gonchigars@gmail.com>
2024-08-07 05:52:40 +0530

    adding caching Day 7

--- src/main/java/com/example/demo/service/BookService.java
+++ src/main/java/com/example/demo/service/BookService.java
@@ -3,6 +3,9 @@ package com.example.demo.service;
 import com.example.demo.entity.Book;
 import com.example.demo.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,21 +20,23 @@ public class BookService {
         this.bookRepository = bookRepository;
     }
 
+    @Cacheable(value = "books")
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
 
+    @Cacheable(value = "books", key = "#id")
     public Book getBookById(Long id) {
         return bookRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Book not found"));
     }
 
+    @CachePut(value = "books", key = "#book.id")
     public Book createBook(Book book) {
         return bookRepository.save(book);
     }
 
-    
-
+    @CacheEvict(value = "books", key = "#id")
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
