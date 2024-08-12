91e744fdf19d3ec54805f5a7afc27681fb76b661
nirajpavankumar <nirajpavankumar1282@gmail.com>
2024-08-12 22:11:51 +0530

    ok

--- src/main/java/com/example/demo/scheduletask/BookCleanupScheduler.java
+++ src/main/java/com/example/demo/scheduletask/BookCleanupScheduler.java
@@ -1,12 +1,15 @@
 package com.example.demo.scheduletask;
 
 import com.example.demo.repository.BookRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.aggregation.ArithmeticOperators.Log;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
+import java.util.logging.Logger;
 
 @Component
 public class BookCleanupScheduler {
@@ -22,9 +25,17 @@ public class BookCleanupScheduler {
     @Scheduled(cron = "0/30 * * * * ?")
     public void cleanUpOldBooks() {
         LocalDate tenYearsAgo = LocalDate.now().minus(10, ChronoUnit.YEARS);
-        bookRepository.findAll().stream()
+          long count=bookRepository.findAll().stream()
                 .filter(book -> book.getPublishedDate().isBefore(tenYearsAgo))
-                .forEach(book -> bookRepository.delete(book));
-        System.out.println("Old books cleaned up successfully");
+                .peek(book -> bookRepository.delete(book))
+                 .count();
+     if(count>0){
+        logger.info("old books cleaned up successfully.Total deleted:{}",count);
+
+      } else{ 
+      Logger.info("No old books found to be cleaned up");
+    }
+
+        
     }
 }
